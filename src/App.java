04af115de74d12e56ea4374d908e73e4c147db6d
TheisenLucas <lucas.theisen@hotmail.com>
2020-03-07 12:13:01 -0300

    alterei app.java

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ public class App {
     public static void main(String[] args) {    //através desta sintaxe o programa começa a ler o código
         System.out.println("Hello World");      //imprime na tela - println = sempre nova linha
         System.out.println("GoodBye World");
+        System.out.println("Hello Again");
     }
 
 }
