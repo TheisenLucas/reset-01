896c645bd9e859b2535de0d412f474b836463241
TheisenLucas <lucas.theisen@hotmail.com>
2020-03-07 10:58:05 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+
+    public static void main(String[] args) {    //através desta sintaxe o programa começa a ler o código
+        System.out.println("Hello World");      //imprime na tela - println = sempre nova linha
+        System.out.println("GoodBye World");
+    }
+
+}
